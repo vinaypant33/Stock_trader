05d4a41c1805fd36a3e14e694a5959d63f439139
Vinay Pant <vinaypant24@gmail.com>
2023-03-13 08:01:03 +0530

    Icons added

---
+++ View/assets.py
@@ -0,0 +1,53 @@
+from PIL import Image, ImageTk
+
+
+
+history_icon  = "View\History.png"
+settings_icon = "View\Settings.png"
+home_icon = "View\home.png"
+
+# home_image  = Image.open(r"View\home.png")
+# home_resized  = home_image.resize((100 ,100) ,Image.LANCZOS)
+# home_final_image  = ImageTk.PhotoImage(home_resized)
+
+# settings_image  = Image.open(settings_icon)
+# settings_resized  = settings_image.resize(100,100)
+# settings_final_image  = ImageTk.PhotoImage(settings_resized)
+
+# history_image  = Image.open(history_icon)
+# history_resized = history_image.resize(100,100)
+# history_final_image = ImageTk.PhotoImage(history_resized)
+
+
+# img = Image.open("View\home.png")
+
+# img = img.resize((100, 100),Image.LANCZOS)
+# img_tk = ImageTk.PhotoImage(img)
+
+# from tkinter import *
+# from PIL import Image
+
+# # Create a Tkinter window
+# root = Tk()
+
+# # Load the image using PIL
+# img = Image.open("View\home.png")
+
+# # Resize the image
+# width, height = img.size
+# img = img.resize((30,30))
+
+# # Convert the PIL image object to a Tkinter PhotoImage object
+# img_tk = ImageTk.PhotoImage(img)
+
+# # Create a Tkinter label widget to display the image
+# label = Label(root, image=img_tk)
+
+# # Set the label to the resized image
+# label.image = img_tk
+
+# # Pack the label widget onto the Tkinter window
+# label.pack()
+
+# # Run the Tkinter event loop
+# root.mainloop()
--- View/dashboard.py
+++ View/dashboard.py
@@ -2,11 +2,12 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter.ttk import *  
+from PIL import Image, ImageTk
 
 # Importing Project made modules
 import colors
 import fonts
-
+import assets
 
 
 
@@ -30,6 +31,14 @@ class Dashboard():
 
         # Defining Constants 
         self.sidebar_maximized  =  True
+
+        # Importing the image files 
+        home_image  = Image.open(r"View\home.png")
+        home_resized  = home_image.resize((1000 ,1000))
+        self.home_final_image  = ImageTk.PhotoImage(home_resized)
+
+
+        
 
     ## Functions for the general work 
     def closing_app(self):
@@ -73,21 +82,22 @@ class Dashboard():
 
     # Function to maximize and minimize the sidebar
     def sidebar_max_min(self):
-        # if self.sidebar_maximized == True:
-        #     messagebox.showinfo("sdf" , "sdffsds")
-        #     self.sidebar_maximized = False
-        # elif self.sidebar_maximized == False:
-        #     messagebox.showerror("sdf" , "I am not working")
         if self.sidebar_maximized == False:
             self.sidebar.configure(width=150)
             self.sidebar_maximized = True
             self.open_close_button.place(x =111 , y = 2)
+            self.home_button.configure(width=16 ,  image=self.home_final_image)
+            self.settings_button.configure(width=16)
+            self.history_button.config(width=16)
+            
         elif self.sidebar_maximized == True:
             self.sidebar_maximized = False
             self.sidebar.configure(width=71)
             self.open_close_button.place(x=30 , y=2 )
-            self.home_button.config(width=7)
-            self.home_button.place(x=0 , y=50)
+            # self.home_button.config(width=7)
+            
+            self.settings_button.config(width=7)
+            self.history_button.config(width=7)
 
     def defining_controls(self):
         ## Defining frames - for the controls
@@ -107,7 +117,7 @@ class Dashboard():
         #### Width 100 and height 676 would be chnaged in the main function and also
         self.open_close_button   =tk.Button(self.sidebar , text="+==+" , bd = 0 , activebackground=colors.sky_blue , activeforeground=colors.white_backgrond , foreground=colors.color_red , background=colors.dark_grey , height=1 , command=self.sidebar_max_min)
         self.home_button  = tk.Button(self.sidebar , text="Home", font=fonts.large_font , background=colors.color_red , activebackground=colors.some_dark_blue , relief=tk.SUNKEN , bd=0 , foreground=colors.white_backgrond , height=1 , width=16
-                                      ,activeforeground='white' , command=self.sidebar_max_min)
+                                      ,activeforeground='white')
         self.settings_button  =tk.Button(self.sidebar , text="Settings", font=fonts.large_font , background=colors.color_red , activebackground=colors.some_dark_blue , relief=tk.SUNKEN , bd=0 , foreground=colors.white_backgrond , height=1 , width=16
                                       ,activeforeground='white')
         self.history_button = tk.Button(self.sidebar , text="History" , bd=0 , activebackground=colors.some_dark_blue , activeforeground=colors.white_backgrond , background=colors.color_red
